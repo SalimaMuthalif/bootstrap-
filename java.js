29a4a6eba7c1ee2b3f41d6d6578e2679b177db14
SalimaMuthalif <salimsalimam761@gmail.com>
2024-09-21 12:13:37 +0530

    first commit

---
+++ java.js
@@ -0,0 +1,43 @@
+const i = 24
+var h = "5"
+var b = 8
+let c = 8
+
+let d = "8"
+ var a=c*b
+ const e= i/c+h
+ let f = b+h*i
+ let x = "apple"
+ let y = "banana"
+
+//  console.log(y,x);
+// console.log(a);
+// console.log(e);
+// console.log(f);
+// console.log(c>=d);
+// console.log(h>b);
+
+if(b==c){
+    console.log("b is equal to c");
+}else{
+    console.log("b is not equal to c");
+    
+}
+if(x!==y){
+    console.log("yes");
+    
+}else{
+    console.log("no");
+    
+}
+const array=[[1,2,3],[6,7,8]]
+console.log(array);
+var object=({name:"salima", role:"developer"})
+console.log(object);
+var object=({age:20,qalify:"degree"})
+console.log(object);
+
+
+
+
+
